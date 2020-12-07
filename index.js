93075a08258aa5a9f1307e1f9751719955fbad6f
jepidoptera <39465008+jepidoptera@users.noreply.github.com>
2020-12-07 17:13:44 -0600

    use heroku's port

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const http = require('http');
 const path = require('path')
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-http.createServer(app).listen(3000);
+http.createServer(app).listen(port);
