1cc3e39160337d593593e8d4fb878bee28db6510
jepidoptera <39465008+jepidoptera@users.noreply.github.com>
2020-12-12 18:52:37 -0600

    handle text and hyperlinks in a unified manner

--- public/extend.js
+++ public/extend.js
@@ -22,46 +22,15 @@ function handlePaste (e) {
         })
         return linksList;
     }
-    // modify html hyperlinks
-    const links = parseLinks($('#bufferDiv'))
-    let textPosition = 0;
-    let parentText = null;
-    console.log(links)
-    links.forEach(link => {
-        // make sure it is not already bracketed
-        let parent = $(link).parent();
-        if ($(link).parent().text() != parentText) {
-            parentText = $(link).parent().text();
-            textPosition = 0;
-        }
-        let linkLoc = parentText.indexOf($(link).text(), textPosition);
-        textPosition = linkLoc;
-        if (parentText[linkLoc-1] === "[" && parentText[linkLoc + $(link).text().length] === "]")
-            return;
-        // assuming it is not, modify it
-        $(link).replaceWith($("<span>")
-            .addClass('modified')
-            .html(`[<a href=${link.href}>${link.text}</a>](${link.href})`)
-        )
-        parentText = $(parent).text()
-    })
 
-    // find all text nodes
-    // var getTextNodesIn = function(el) {
-    //     return $(el).find("*").filter(function() {
-    //         return this.nodeType == 3;
-    //     });
-    // };
-    function getTextNodesIn(node, includeWhitespaceNodes) {
-        var textNodes = [], nonWhitespaceMatcher = /\S/;
+    function getTextNodesIn(node) {
+        var textNodes = []
     
         function getTextNodes(node) {
             if (node.nodeType == 3) {
-                if (includeWhitespaceNodes || nonWhitespaceMatcher.test(node.nodeValue)) {
-                    textNodes.push(node);
-                }
+                textNodes.push(node);
             } else {
-                for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+                for (let i = 0, len = node.childNodes.length; i < len; ++i) {
                     getTextNodes(node.childNodes[i]);
                 }
             }
@@ -72,7 +41,7 @@ function handlePaste (e) {
     }
 
     // find links that are present in text
-    const textNodes = getTextNodesIn($("#bufferDiv")[0], true);
+    const textNodes = getTextNodesIn($("#bufferDiv")[0]);
     const urlRegex = new RegExp(/(http[s]?:\/\/)?[-a-z0-9@:%._\+~#=]{1,256}\.(com|org|io|me){1}\b([-a-z0-9()@:%_\+.~#?&//=]*)[^.\s]/gi)
     const markdownRegex = new RegExp(/(\[.*?\]\(.*?\))/gi)
     const markdowns = Array.from($("#bufferDiv").text().matchAll(markdownRegex))
@@ -83,11 +52,28 @@ function handlePaste (e) {
             // get the length of the element's text before we modify it
             let elementLength = node.textContent.length;
             // is there a link in here?
-            let urls = node.textContent.matchAll(urlRegex)
+            let urls = Array.from(node.textContent.matchAll(urlRegex))
             let newHTML = ''
             let textPosition = 0
 
+            // furthermore, if this element itself is inside a link, we must consider that
+            let ancestors = ((node) => {
+                return $(node).parent().length 
+                    ? [$(node)].concat(ancestors($(node).parent())) 
+                    : [$(node)]
+                })
+            let ancestralLink = ancestors(node).filter(node => node[0].nodeName === "A")[0]
+            if (ancestralLink) {
+                urls.push({
+                    0: ancestralLink[0].href,
+                    text: ancestralLink.text(),
+                    index: 0
+                })
+                //
+                node = ancestralLink[0]
+            }
             for (url of urls) {
+                if (!url.text) url.text = url[0]
                 // but is it in markdown format already?
                 for (markdown of markdowns) {
                     if (url.index + elementPosition > markdown.index && url.index + elementPosition < markdown.index + markdown[0].length) {
@@ -98,7 +84,7 @@ function handlePaste (e) {
                 }
                 if (!url.ignore) {
                     newHTML += node.textContent.slice(textPosition, url.index)
-                    newHTML += `<span class='modified'>[<a href='${url[0]}'>${url[0]}</a>](${url[0]})</span>`
+                    newHTML += `<span class='modified'>[<a href='${url[0]}'>${url.text}</a>](${url[0]})</span>`
                     textPosition = url.index + url[0].length
                 }
             }
